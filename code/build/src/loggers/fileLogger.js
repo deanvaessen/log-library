a77fc5f22c2dca1e479d136dfbed8feab883e27b
Dean Vaessen <hello@deanvaessen.com>
2016-11-12 00:33:32 +0100

    Ran an npm run build

--- code/build/src/loggers/fileLogger.js
+++ code/build/src/loggers/fileLogger.js
@@ -1,6 +1,6 @@
 /*******************************
- * [_consoleLogger.js]
- * Define the consoleLogger function
+ * [_fileLogger.js]
+ * Define the fileLogger function
  ******************************/
 
 /**
@@ -113,7 +113,6 @@
 
 			// Define a base file to verify the size of the mostRecentFile
 			let logFileExtention = '.txt',
-				//mostRecentArchivedLogPath,
 				baseFileName = 'log' + logFileExtention,
 				baseLogPath = writePath + baseFileName,
 				newlyArchivedLogNumber = logsAmount + 1,
@@ -121,7 +120,7 @@
 				newlyArchivedFileLogPath = writePath + newlyArchivedLogFileName,
 				messageWithLineBreak = wrapLineBreak(messageContent);
 
-			// Is there a mostRecentFile or is this the first item?
+			// Is there already a log file or is this the first item?
 			if (logsAmount != 0){
 				const fileStats = fs.statSync(baseLogPath),
 						fileSizeInBytes = fileStats['size'];
